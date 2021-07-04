6c3ad704bc87412c7751509b368db21d71a66de2
dug <etan@ctemplar.com>
2021-07-05 00:47:10 +0100

    optimise and clean up code.

--- cl_nitro.lua
+++ cl_nitro.lua
@@ -1,76 +1,59 @@
-local NitroCooldown = 0
+vehicle_model = "bmx" -- { YOUR SPAWN CODE OF VEHICLE HERE}
 
-function ShowNotification( text )
+RegisterCommand("nitro", function()
+    TriggerServerEvent("CheckNitro")
+end)
+
+function notify(text)
     SetNotificationTextEntry("STRING")
     AddTextComponentSubstringPlayerName(text)
     DrawNotification(false, false)
 end
 
-RegisterCommand("nitro", function()
-    if NitroCooldown == 0 then
-        if IsPedInAnyVehicle(PlayerPedId(), true) then
-            ShowNotification("~r~You can't be in a vehicle!")
-        else
-            TriggerServerEvent("CheckNitro")
-        end
-    else
-        ShowNotification("~r~ Stop Spamming please!")
-    end
+RegisterNetEvent("nitro_notify")
+AddEventHandler("nitro_notify", function(text)
+    notify(text)
 end)
 
-RegisterNetEvent("NotBoosing")
-AddEventHandler("NotBoosing", function()
-    ShowNotification("~r~You are not a nitro booster!")
-end)
-
-RegisterNetEvent("NoGuild")
-AddEventHandler("NoGuild", function()
-    ShowNotification("Discord doesnt seem to be installed/running.")
-end)
-
+nitro_cooldown = 0
 RegisterNetEvent("SpawningNitro")
 AddEventHandler("SpawningNitro", function()
     local pid = PlayerPedId()
-    if IsPedDeadOrDying(PlayerPedId(), 1) then 
-        ShowNotification("You are dead!")
+    if IsPedInAnyVehicle(pid, true) then
+        notify("~r~You can't be in a vehicle!")
         return
     end
-    if NitroCooldown == 0 then
-        local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 8.0, 0.5))
-        local veh = bmx
-        if veh == nil then veh = "bmx" end
-        NitroCooldown = 15
-        vehiclehash = GetHashKey(veh)
-        RequestModel(vehiclehash)
-        
-        Citizen.CreateThread(function() 
-            local waiting = 0
-            while not HasModelLoaded(vehiclehash) do
-                waiting = waiting + 100
-                Citizen.Wait(100)
-                if waiting > 5000 then
-                    ShowNotification("~r~Could not load the vehicle model in time, a crash was prevented.")
-                    break
-                end
+    if IsPedDeadOrDying(pid, 1) then 
+        notify("~r~Can't do that when your dead.")
+        return
+    end
+    if nitro_cooldown == 0 then
+        nitro_cooldown = 15
+        local modelHash = GetHashKey(vehicle_model)
+        if not HasModelLoaded(modelHash) then
+            RequestModel(modelHash)
+            while not HasModelLoaded(modelHash) do
+                Wait(0)
             end
-            TaskStartScenarioInPlace(pid, 'WORLD_HUMAN_HAMMERING', false, true)
-            FreezeEntityPosition(pid, true)
-            Citizen.Wait(6000)
-            ClearPedTasks(pid)
-            FreezeEntityPosition(pid, false)
-            CreateVehicle(vehiclehash, x, y, z, GetEntityHeading(PlayerPedId())+90, 1, 0)
-            ShowNotification("~g~ Spawned - Thanks for boosting!")
-        end)
+        end
+        TaskStartScenarioInPlace(pid, "WORLD_HUMAN_HAMMERING", false, true)
+        FreezeEntityPosition(pid, true)
+        Citizen.Wait(6000)
+        ClearPedTasks(pid)
+        FreezeEntityPosition(pid, false)
+        local vehicle = CreateVehicle(modelHash, GetEntityCoords(pid), GetEntityHeading(pid), true, false)
+        SetVehicleOnGroundProperly(vehicle)
+        SetPedIntoVehicle(pid, vehicle, -1)
+        notify("~g~Spawned - Thanks for boosting!")
     else
-        ShowNotification("~r~Stop Spamming!!!")
+        notify("~r~You can spawn another "..vehicle_model.." in "..nitro_cooldown.." seconds.")
     end
 end)
 
-
 Citizen.CreateThread(function()
     while true do
-        if NitroCooldown > 0 then
-            NitroCooldown = NitroCooldown - 1
+        if nitro_cooldown > 0 then
+            nitro_cooldown = nitro_cooldown - 1
         end
         Wait(1000)
     end
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,9 @@
-fx_version 'bodacious'
-games { 'gta5', 'rdr3' }
-
+fx_version 'cerulean'
+games { 'gta5', }
 author 'Blaze'
 description 'Checks if they have a role then spawns a bmx if so'
 version '1.0.0'
 
-client_scripts{
-  'cl_nitro.lua',
-}
-
-server_scripts {
-  'sv_nitro.lua',
-}
+client_scripts{ 'cl_nitro.lua', }
+server_scripts { 'sv_nitro.lua', }
 
